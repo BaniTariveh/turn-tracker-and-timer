5aa1960386a5cd89ed9b92839caa84a44d3b01f1
Bani Tariveh <bani.tariveh@gmail.com>
2024-12-25 15:54:02 +0200

    declarations and stubs

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,14 @@
 // Overtime light
 #define PIN_OTLIGHT 6
 
+#define TURN_LENGTH 60000
+
 // put function declarations here:
 
+void waitSecond();
+void showWLight();
+void showOTLight();
+void toggleLight(int pin);
 
 void setup() {
   pinMode(PIN_NT, INPUT_PULLUP);
@@ -23,3 +29,20 @@ void loop() {
 }
 
 // put function definitions here:
+
+void waitSecond()
+{
+  delay(1000);
+}
+
+void showWLight()
+{
+}
+
+void showOTLight()
+{
+}
+
+void toggleLight(int pin)
+{
+}
