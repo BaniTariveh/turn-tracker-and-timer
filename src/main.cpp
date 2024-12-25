352ff8b21589acca8df29280765ed4e7b3043019
Bani Tariveh <bani.tariveh@gmail.com>
2024-12-25 15:41:39 +0200

    Stub

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,24 @@
 #include <Arduino.h>
 
+// Next turn
+#define PIN_NT 4
+// Warning light
+#define PIN_WLIGHT 5
+// Overtime light
+#define PIN_OTLIGHT 6
+
 // put function declarations here:
 
 
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(PIN_NT, INPUT_PULLUP);
+  pinMode(PIN_WLIGHT, OUTPUT);
+  pinMode(PIN_OTLIGHT, OUTPUT);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // 
   
 }
 
